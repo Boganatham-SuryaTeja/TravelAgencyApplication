ae1d9be5470def0cedb4863f7cfcdc9b43c1a6fe
Boganatham-SuryaTeja <100075610+Boganatham-SuryaTeja@users.noreply.github.com>
2023-10-11 20:36:03 -0700

    Create about.php

---
+++ api/about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About - Our Website</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Create a CSS file for styling -->
+</head>
+<body>
+    <header>
+        <h1>About Us</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section class="about-us">
+    <h2>About Wonderlust Adventures</h2>
+        <p>
+            Wonderlust Adventures is a leading travel agency dedicated to creating memorable travel experiences. We have a passionate team of explorers who are committed to making your travel dreams a reality.
+        </p>
+    </section>
+
+    <section class="team">
+        <h2>Our Team</h2>
+        <ul>
+            <li>John Doe - CEO</li>
+            <li>Jane Smith - Designer</li>
+            <li>Bob Johnson - Developer</li>
+        </ul>
+    </section>
+
+    <footer>
+        &copy; 2023 Wonderlust Adventures. All rights reserved.
+    </footer>
+</body>
+</html>
