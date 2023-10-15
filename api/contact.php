d0e948d8a6a6a6d3635407e096a04e168caaaf1f
Boganatham-SuryaTeja <100075610+Boganatham-SuryaTeja@users.noreply.github.com>
2023-10-14 19:21:40 -0700

    Update contact.php

--- api/contact.php
+++ api/contact.php
@@ -25,7 +25,8 @@
         </p>
         <?php
         $contactFile = './contacts.txt';
-        echo "<p>$contactFile</p>";
+        $contactFile1 = file_get_contents("contacts.txt");
+        echo "<p>$contactFile1</p>";
         if (file_exists($contactFile)) {
             $lines = file($contactFile);
             
